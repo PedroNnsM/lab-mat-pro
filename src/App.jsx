74656bbd0620506a06d075592b4b601e876e0af6
PedroNnsM <pedronunes95@outlook.com>
2023-04-30 23:21:23 -0300

    Colocando map nos itens da nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { Header } from "./components/Header";
 
-
 function App() {
   return (
-    <div className="flex min-h-screen bg-slate-700 flex-col max-w-full">
-
-      <div className="mx-auto my-0 bg-blue-400 max-w-4xl">
-      <Header />
-        <h1 className="w-full">
-          Hello Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Sapiente, aliquam reiciendis. Maiores in natus corporis accusamus eos
-          quisquam, delectus laudantium repellat voluptatibus, debitis ab
-          molestias. Rerum esse dignissimos expedita quo!!!
-        </h1>
+    <div className="flex min-h-screen bg-slate-700 flex-col max-w-full  bg-opacity-40  ">
+      <div className="mx-auto my-0 bg-gradient-to-br from-blue-500 from-10%  via-blue-800 to-blue-900 to-100%">
+        <div className="">
+          <Header />
+          <h1 className="w-4xl">
+            Hello Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Sapiente, aliquam reiciendis. Maiores in natus corporis accusamus
+            eos quisquam, delectus laudantium repellat voluptatibus, debitis ab
+            molestias. Rerum esse dignissimos expedita quo!!!
+          </h1>
+        </div>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,48 @@
-import Logo from '../assets/Logo.svg'
+import Logo from "../assets/Logo.svg";
 
-export function Header(){
-  return(
-    <div className="flex justify-center items-center">
-    <img className='w-[25rem]' src={Logo} alt="" />
+export function Header() {
+  const navList = [
+    {
+      id: 1,
+      name: "home",
+      text: "Home",
+    },
+    {
+      id: 2,
+      name: "about",
+      text: "Quem somos",
+    },
+    {
+      id: 3,
+      name: "team",
+      text: "Equipe",
+    },
+    {
+      id: 4,
+      name: "services",
+      text: "Serviços",
+    },
+    {
+      id: 5,
+      name: "trainings",
+      text: "Treinamentos",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col justify-center items-center ">
+      <img className="w-[25rem]" src={Logo} alt="" />
+      <nav>
+        <ul className="flex gap-6 mt-[-20px]">
+          {navList.map((item) => {
+            return (
+              <li key={item.idid}>
+                <a href={`/${item.name}`} className="text-WHITE hover:text-opacity-60" >{item.text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
