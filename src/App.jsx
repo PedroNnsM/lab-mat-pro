2b7331a7ad8bfda7d2780106853076b8203dce1e
PedroNnsM <pedronunes95@outlook.com>
2023-04-30 21:53:54 -0300

    Iniciando os principais componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-
 function App() {
   return (
-    <>
-      <h1 className="bg-slate-700">Hello world!!</h1>
-    </>
+    <div className="flex min-h-screen bg-slate-700 flex-col max-w-full">
+      <div className="mx-auto my-0 bg-blue-400 max-w-4xl">
+        <h1 className="w-full">
+          Hello Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Sapiente, aliquam reiciendis. Maiores in natus corporis accusamus eos
+          quisquam, delectus laudantium repellat voluptatibus, debitis ab
+          molestias. Rerum esse dignissimos expedita quo!!!
+        </h1>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'desktop-tight': '1366px',
+        mobile: { min: '320px', max: '767px' },
+        'sm-mid': '480px',
+        sm: '640px',
+        md: { min: '768px', max: '1023px' },
+        lg: { min: '1024px', max: '1365px' },
+      },
+    },
   },
   plugins: [],
 };
