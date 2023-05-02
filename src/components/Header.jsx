70dbb29e6bf50d224db9670bed064a7d888718a7
PedroNnsM <pedronunes95@outlook.com>
2023-05-01 23:55:51 -0300

    icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,68 +1,88 @@
+import { useState } from "react";
 import Logo from "../assets/Logo.svg";
+import { FiMenu } from "react-icons/fi";
+import { RiCloseFill } from "react-icons/ri";
 
 export function Header() {
+  const [open, setOpen] = useState();
   const navList = [
     {
-      id: '1',
+      id: "1",
       name: "home",
       text: "Home",
     },
     {
-      id: '2',
+      id: "2",
       name: "quem-somos",
       text: "Quem somos",
     },
     {
-      id: '3',
+      id: "3",
       name: "equipe",
       text: "Equipe",
     },
     {
-      id: '4',
+      id: "4",
       name: "servicos",
       text: "Serviços",
     },
     {
-      id: '5',
+      id: "5",
       name: "treinamentos",
       text: "Treinamentos",
     },
     {
-      id: '6',
+      id: "6",
       name: "projetos",
       text: "Projetos",
     },
     {
-      id: '7',
+      id: "7",
       name: "publicacoes",
       text: "Publicações",
     },
     {
-      id: '8',
+      id: "8",
       name: "clientes",
       text: "Clientes",
     },
     {
-      id: '9',
+      id: "9",
       name: "contatos",
       text: "Contatos",
     },
   ];
 
   return (
-    <div className="flex flex-col justify-center items-center w-full ">
+    <header className="flex  flex-col  justify-center items-center w-full ">
       <img className="w-[25rem]" src={Logo} alt="" />
+      {open ? (
+        <RiCloseFill
+          className="lg:hidden block h-8 w-8 text-WHITE cursor-pointer"
+          onClick={() => setOpen(!open)}
+        />
+      ) : (
+        <FiMenu
+          className="lg:hidden block h-8 w-8 text-WHITE cursor-pointer"
+          onClick={() => setOpen(!open)}
+        />
+      )}
       <nav>
-        <ul className="flex gap-6 mt-[-20px] mb-10 md:flex-col">
+        <ul className="flex gap-6 mt-[-20px] mb-10 sm:flex-col lg:flex-row md:flex-row ">
           {navList.map((item) => {
             return (
               <li key={item.id}>
-                <a href={`/${item.name}`} className="text-WHITE hover:text-opacity-60 font-sans	" >{item.text}</a>
+                <a
+                  href={`/${item.name}`}
+                  className="text-WHITE hover:text-opacity-60 font-sans	"
+                >
+                  {item.text}
+                </a>
               </li>
             );
           })}
         </ul>
       </nav>
-    </div>
+    </header>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,20 @@
 import { Header } from "../components/Header";
 
-
 function Home() {
   return (
     <div className=" min-h-screen bg-slate-700 flex-col w-full  bg-opacity-40  ">
       <div className="flex flex-col items-center bg-gradient-to-br from-blue-500 from-10%  via-blue-800 to-blue-900 to-100% ">
         <div className="w-xl">
           <Header />
-          qweqeqe
+          <section className="text-WHITE text-center">
+            <h2 className=" text-3xl">
+              Inovação através da pesquisa e tecnologia
+            </h2>
+            <p className=" text-xl">
+              Laboratório especializado em análise de materiais e capacitação
+              para indústrias.
+            </p>
+          </section>
         </div>
       </div>
     </div>
