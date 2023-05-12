06058fb1fb3ee2a0bcf3c081a02bf60fdd0316e0
PedroNnsM <pedronunes95@outlook.com>
2023-05-11 23:00:51 -0300

    Endereço e logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import { Logo } from "../assets/Logo";
-import logo from "../assets/Logo.svg";
 
 export function Footer() {
   return (
-    <footer className="bg-black-900">
-      <Logo width={'250px'} />
+    <footer className=" flex items-center justify-center  h-60 bg-gradient-to-br relative from-blue-500 from-10%  via-blue-800 to-blue-900 to-100% ">
+      <div className="w-full flex justify-between">
+        <Logo width={"350px"} />
+        <div className="flex flex-col items-end ">
+          <p>Endereço: Rua Exemplo, 123 - Bairro - Cidade - Estado - CEP</p>
+          <p>Telefone: (00) 0000-0000</p>
+          <p>E-mail: exemplo@empresa.com.br</p>
+        </div>
+      </div>
     </footer>
   );
 }
