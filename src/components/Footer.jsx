a10bc9f26ac4a2daa7d527f4f85d99a150642855
PedroNnsM <pedronunes95@outlook.com>
2023-05-11 23:14:31 -0300

    Separação dos itens no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import { Logo } from "../assets/Logo";
 
 export function Footer() {
   return (
-    <footer className=" flex items-center justify-center  h-60 bg-gradient-to-br relative from-blue-500 from-10%  via-blue-800 to-blue-900 to-100% ">
-      <div className="w-full flex justify-between">
+    <footer className="w-full flex items-center justify-center h-60 bg-gradient-to-br relative from-blue-500 from-10%  via-blue-800 to-blue-900 to-100% ">
+      <div className=" flex justify-between w-[80%]">
         <Logo width={"350px"} />
-        <div className="flex flex-col items-end ">
+        <div className="flex flex-col items-end text-WHITE font-semibold">
           <p>Endereço: Rua Exemplo, 123 - Bairro - Cidade - Estado - CEP</p>
           <p>Telefone: (00) 0000-0000</p>
           <p>E-mail: exemplo@empresa.com.br</p>
