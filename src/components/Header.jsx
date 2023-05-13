32da315abecde000458fe9ed0cedd4f0b2b8727f
PedroNnsM <pedronunes95@outlook.com>
2023-05-13 16:53:57 -0300

    Ajustando o Header para que fique uniforme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -54,25 +54,31 @@ export function Header() {
   ];
 
   return (
-    <header className="flex  flex-col  justify-center items-center max-w-6xl ">
+    <header className="flex  flex-col  justify-center items-center max-w-6xl  bg-gradient-to-br w-full from-blue-500 from-10%  via-blue-800 to-blue-900 to-100%">
       <img className="w-[25rem]" src={Logo} alt="" />
-      {open ? (
-        <RiCloseFill
-          className="block h-8 w-8 text-WHITE cursor-pointer
+      <div className="block mb-8 ">
+        {open ? (
+          <RiCloseFill
+            className="block h-8 w-8 text-WHITE cursor-pointer absolute right-10 top-10
           md:hidden 
           "
-          onClick={() => setOpen(!open)}
-        />
-      ) : (
-        <FiMenu
-          className="block h-8 w-8 text-WHITE cursor-pointer
+            onClick={() => setOpen(!open)}
+          />
+        ) : (
+          <FiMenu
+            className="block h-8 w-8 text-WHITE cursor-pointer absolute right-10 top-10
           md:hidden 
           "
-          onClick={() => setOpen(!open)}
-        />
-      )}
-      <nav>
-        <ul className="flex gap-6 mt-[-20px] mb-10 flex-col lg:flex-row md:flex-row ">
+            onClick={() => setOpen(!open)}
+          />
+        )}
+      </div>
+      <nav className="translate-y-[-150]">
+        <ul
+          className={`flex gap-6 items-center lg:block mt-[-20px] mb-10 flex-col lg:flex-row md:flex-row  ${
+            open ? "block" : "hidden"
+          }`}
+        >
           {navList.map((item) => {
             return (
               <li key={item.id}>
