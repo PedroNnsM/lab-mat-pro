1ad45b220c533b7dc7f8166e97f26a61d11848ae
PedroNnsM <pedronunes95@outlook.com>
2023-05-18 23:38:23 -0300

    feat: page service

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -18,7 +18,7 @@ export default function Services() {
       description:
         "A medição da força necessária para deformar um corpo de prova fornece informações sobre propriedades do material, como Resistência à Compressão, Módulo de Elasticidade, Tensão de Escoamento e Deformação sob Compressão. Esse tipo de ensaio é amplamente utilizado na indústria de construção civil em materiais como concreto armado, madeira e ferro fundido. Para avaliar se esse tipo de ensaio é ou não ideal para a finalidade desejada, é necessário compreender o funcionamento e a função desse tipo de ensaio.",
 
-      img: `${ equipamento1 }`,
+      img: `${equipamento1}`,
       equipament: "Máquina universal de ensaios mecânicos Instron 5984",
     },
     {
@@ -26,7 +26,7 @@ export default function Services() {
       title: "Ensaio dinâmico mecânico (DMA)",
       description:
         "O DMA é uma importante técnica usada para medir as propriedades mecânicas e viscoelásticas de materiais, como termoplásticos, termorrígidos, elastômeros, cerâmica e metais.A tecnologia DMA é a solução perfeita quando a precisão máxima é exigida e o material deve ser caracterizado em uma ampla faixa de rigidez e/ou de frequência. Além disso, a tecnologia DMA é extremamente versátil e, portanto, o DMA pode caracterizar materiais, mesmo em líquidos ou em níveis de umidade relativa específica.",
-      img: `${ equipamento2 }`,
+      img: `${equipamento2}`,
       equipament:
         "Equipamento de teste de propriedade dinâmico mecânicas DMA 242 NETZSCH",
     },
@@ -36,7 +36,7 @@ export default function Services() {
       description:
         "A realização do ensaio de dureza e microdureza é indispensável para determinar a dureza de materiais, com as mais diversas formas e tamanhos, e com as superfícies termicamente tratadas, para determinação da profundidade da camada efetiva endurecida por têmpera superficial, camadas cementradas e nitretadas.  O ensaio de dureza e microdureza também é recomendado para análise de materiais de grande porte, além de peças forjadas e peças fundidas.",
 
-      img: `${ equipamento3 }`,
+      img: `${equipamento3}`,
       equipament: "Equipamento de Nano Identação FISCHERSCOPE HM 2000",
     },
     {
@@ -45,7 +45,7 @@ export default function Services() {
       description:
         "Através da realização desse ensaio, pode-se determinar a especificação de um material, assim como possíveis causas de desgastes prematuros, fraturas ou outras falhas. Dentro disso, ela assume um papel muito importante na compreensão do comportamento dos metais.",
 
-      img: `${ equipamento4 }`,
+      img: `${equipamento4}`,
       equipament: "",
     },
     {
@@ -54,7 +54,7 @@ export default function Services() {
       description:
         "Determinar percentual de cada componente químico presente na amostra através de espectroscopia, análise química de Metais",
 
-      img: `${ equipamento5 }`,
+      img: `${equipamento5}`,
       equipament:
         "Equipamento de teste de propriedade dinâmico mecânicas DMA 242 NETZSCH",
     },
@@ -63,7 +63,7 @@ export default function Services() {
       title: "Ensaios Climáticos",
       description: "Temperatura, humidade, corrosão por nevoeiro salino",
 
-      img: `${ equipamento6 }`,
+      img: `${equipamento6}`,
       equipament: "",
     },
   ];
@@ -75,17 +75,19 @@ export default function Services() {
           <Header />
         </div>
       </div>
-      <section className="flex items-center w-xl">
-        <h1>Serviços</h1>
+      <section className="flex flex-col items-start max-w-4xl">
+        <h1 className="font-bold text-blue-800 text-3xl my-9">Serviços</h1>
+
         {servicos.map((service) => {
-          return(
-          <Equipaments
-            key={service.id}
-            title={service.title}
-            description={service.description}
-            img={service.img}
-            equipament={service.equipament}
-          />)
+          return (
+            <Equipaments
+              key={service.id}
+              title={service.title}
+              description={service.description}
+              img={service.img}
+              equipament={service.equipament}
+            />
+          );
         })}
       </section>
       <Footer />
