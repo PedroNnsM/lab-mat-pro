c3035e0b6d4e072880d20546cb2214f65a46a60c
PedroNnsM <pedronunes95@outlook.com>
2023-07-22 13:30:56 -0300

    marcos

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,6 +4,7 @@ import cvImagem from "../../img/logo-cv.png";
 
 import ImagemCosta from "../../img/imagemCosta.png";
 import ImagemAntonio from "../../img/imagem-antonio.png";
+import ImagemMarcos from "../../img/imagem-marcos.png";
 
 
 export default function Team() {
@@ -24,6 +25,14 @@ export default function Team() {
         "Coordenador do Laboratório Urutau Aerodesign de Estruturas Aeroespaciais da UEA e professor da Escola Superior de Tecnologia da Universidade do Estado do Amazonas (EST-UEA). Tem experiência nas áreas de Administração de Empresas e Engenharia de Materiais aplicada a Biotecnologia, atuando em temas como Materiais Compósitos, Empreendedorismo e Inovação e Gestão de Projetos Industriais. ",
       cv: "http://lattes.cnpq.br/2276542034087411",
     },
+    {
+      id: "3",
+      name: "Dr. Marcos Dantas dos Santos ",
+      imagem: `${ImagemMarcos}`,
+      description:
+        "Tem experiência na área de Engenharia Mecânica e Materiais, com ênfase em Processos de Fabricação atuando principalmente nos seguintes temas: seleção de materiais para processos de fabricação e projetos mecânicos, análise de falhas e danos em materiais, análise de desgaste atrito e lubrificação em tribosistemas.  ",
+      cv: "http://lattes.cnpq.br/6521766717113975",
+    },
   ];
   return (
     <div className=" min-h-screen bg-slate-700 flex-col w-full  bg-opacity-40 flex items-center ">
@@ -40,7 +49,7 @@ export default function Team() {
               key={person.id}
               className="flex items-center justify-between gap-4 border-b pb-6 border-grey-700"
             >
-              <img src={person.imagem} alt={` imagem ${person.name}`} />{" "}
+              <img className="bg-bg-img-dr bg-no-repeat bg-bottom w-[210px] h-[235px]" src={person.imagem} alt={` imagem ${person.name}`} />{" "}
               <div className="flex flex-col ">
                 <div className="flex items-center gap-2">
                   <h2 className="text-black text-lg font-semibold">{person.name}</h2>
