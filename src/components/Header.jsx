fc815aae45baa9a6a875e314bd4adf231eacff87
PedroNnsM <pedronunes95@outlook.com>
2023-10-10 06:30:23 -0600

    alguns testes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,53 @@ import Logo from "../assets/Logo.svg";
 import { FiMenu } from "react-icons/fi";
 import { RiCloseFill } from "react-icons/ri";
 import { BsWhatsapp } from "react-icons/bs";
+import {  NavLink } from "react-router-dom";
 
 export function Header() {
   const [open, setOpen] = useState();
   const navLinks = [
+    {
+      id: "1",
+      name: "",
+      text: "Home",
+    },
+    {
+      id: "2",
+      name: "quem-somos",
+      text: "Quem somos",
+    },
+    {
+      id: "3",
+      name: "equipe",
+      text: "Equipe",
+    },
+    {
+      id: "4",
+      name: "servicos",
+      text: "Serviços",
+    },
+    {
+      id: "5",
+      name: "treinamentos",
+      text: "Treinamentos",
+    },
+    {
+      id: "7",
+      name: "publicacoes",
+      text: "Publicações",
+    },
+    {
+      id: "8",
+      name: "clientes",
+      text: "Clientes",
+    },
+    {
+      id: "9",
+      name: "contato",
+      text: "Contato",
+    },
+  ];
+  const navLinks2 = [
     {
       id: "1",
       name: "",
@@ -88,21 +131,20 @@ export function Header() {
         })}
       </nav>
       {open ? (
-        <div className="lg:hidden">
-          <div className="ox-2 pt-2 pb-3 space-y-1 sm:px-3 flex items-center flex-col transition-all duration-500">
-            {navLinks.map((link) => {
-              return (
-                <a
-                  key={link.id}
-                  href={`/${link.name}`}
-                  className="text-WHITE p-3 hover:bg-blue-900 rounded-md hover:text-opacity-80 font-bold font-sans transition-all duration-500	text-base "
-                >
-                  {link.text}
-                </a>
-              );
-            })}
+        <div className="lg:hidden nox-2 pt-2 pb-3 space-y-1 sm:px-3 flex items-center flex-col transition-all duration-500">
+          {navLinks2.map((href) => {
+          return (
+            <NavLink 
+              key={href.id}
+              href={`/${href.name}`}
+              className="text-WHITE p-3 hover:bg-blue-900 rounded-md hover:text-opacity-80 font-bold font-sans transition-all duration-500	"
+            >
+              {href.text}
+            </NavLink>
+          );
+        })}
           </div>
-        </div>
+        
       ) : null}
     </header>
   );
