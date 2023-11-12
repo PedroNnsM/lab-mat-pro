e583f7d437f97b8282d4034b18656f00ea667d45
PedroNnsM <pedronunes95@outlook.com>
2023-11-12 12:00:16 -0700

    Botoes na parte de baixo, mas falta colocar o index para funcionar

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -7,33 +7,33 @@ import {
 export function Carrousel() {
   const fotos = [
     {
-      id: '1',
+      id: "1",
       img: "https://i.ibb.co/NYB1Hsk/01.png",
       alt: "Imagem 1",
     },
+
     {
-      id: '2',
-      img: "https://i.ibb.co/QHKNpLk/02.png",
-      alt: "Imagem 1",
-    },
-    {
-      id: '3',
+      id: "2",
       img: "https://i.ibb.co/28ms9XW/04.png",
       alt: "Imagem 1",
     },
     {
-      id: '4',
+      id: "3",
       img: " https://i.ibb.co/3SDyhbL/05.png",
       alt: "Imagem 1",
     },
     {
-      id: '5',
+      id: "4",
       img: "https://i.ibb.co/r2ZDrzG/06.png",
       alt: "Imagem 1",
-    },
+    }, // {
+    //   id: "5",
+    //   img: "https://i.ibb.co/QHKNpLk/02.png",
+    //   alt: "Imagem 1",
+    // },
   ];
 
-  let [current, setCurrent] = useState();
+  let [current, setCurrent] = useState(0);
 
   let previousSlide = () => {
     if (current === 0) setCurrent(fotos.length - 1);
@@ -44,14 +44,28 @@ export function Carrousel() {
     if (current === fotos.length - 1) setCurrent(0);
     else setCurrent(current + 1);
   };
+  const goToSlide = (index) => {
+    setCurrent(index);
+  };
+
   return (
     <div className="overflow-hidden relative">
-      <div className={`flex transition ease-out duration-75 `}
-      style={{
-        transform: `translateX(-${current * 100}%)`
-      }}>
+      <div
+        className={`flex transition ease-out duration-75 `}
+        style={{
+          transform: `translateX(-${current * 100}%)`,
+        }}
+      >
         {fotos.map((item) => {
-          return <img className="w-full" src={item.img} alt={item.alt} key={item.id} />;
+          return (
+            <img
+              className="w-full h-full object-cover"
+              src={item.img}
+              alt={item.alt}
+              key={item.id}
+              style={{ objectFit: "cover", objectPosition: "center center" }}
+            />
+          );
         })}
       </div>
       <div className="absolute top-0 h-full w-full justify-between items-center flex z-10 text-WHITE px-4 text-3xl">
@@ -61,6 +75,19 @@ export function Carrousel() {
         <button onClick={nextSlide}>
           <BsFillArrowRightCircleFill />
         </button>
+      </div>
+      <div className=" absolute bottom-0 py-4 flex justify-center w-full gap-3">
+        {fotos.map((s, i) => {
+          return (
+            <div
+              onClick={() => goToSlide(i)}
+              key={"circle" + i}
+              className={` rounded-full w-5 h-5 cursor-pointer border border-blue-400 ${
+                i == current ? "bg-blue-900 " : "bg-WHITE "
+              }`}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
